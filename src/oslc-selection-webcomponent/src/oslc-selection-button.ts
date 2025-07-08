f7d65a81a9a2574da8f90f04a62fda76551e66f3
Andrew Berezovskyi <andrew@berezovskyi.me>
2025-07-09 00:45:41 +0200

    fix: licence

--- src/oslc-selection-webcomponent/src/oslc-selection-button.ts
+++ src/oslc-selection-webcomponent/src/oslc-selection-button.ts
@@ -1,14 +1,7 @@
 /**
- * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2025 Andrew Berezovskyi
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Distribution License 1.0 which is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * SPDX-License-Identifier: BSD-3-Simple
+ * SPDX-License-Identifier: EPL-2.0
  */
 
 import { OslcPostMessageHelper, OslcResource, OslcProtocol } from '@oslc/postmessage-helper';
